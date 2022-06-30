930cafcd459342cdfd868f3386f971370cff531a
tpgus <43470398+tpgus@users.noreply.github.com>
2022-06-30 20:08:58 +0900

    Update areThereDuplicates.js

--- 빈도수/areThereDuplicates.js
+++ 빈도수/areThereDuplicates.js
@@ -19,7 +19,7 @@ function areThereDuplicates1(...args) {
 }
 
 // Solution 2 : Multiple Pointer
-// Time : O(NlogN)
+// Time : O(N)
 // Space : O(1)
 function areThereDuplicates2(...args) {
   args.sort((a, b) => a - b); // 여기여기여기여ㅣ겨ㅣ여ㅣ기 문자열이 왜 정렬이 안 됨?
