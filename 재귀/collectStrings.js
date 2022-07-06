e27b7c88ca726b42ac90c33fe9837896561c5bad
tpgus <tpgus758@naver.com>
2022-07-06 09:04:57 +0900

    중첩된 객체안에 있는 문자열 모으기

--- 재귀/collectStrings.js
+++ 재귀/collectStrings.js
@@ -25,7 +25,7 @@ function collectStrings1(obj) {
   let result = [];
   for (let i in obj) {
     if (typeof obj[i] === "object" && !Array.isArray(obj[i])) {
-      return result.concat(collectStrings(obj[i]));
+      return result.concat(collectStrings1(obj[i]));
     }
     if (typeof obj[i] === "string") {
       result.push(obj[i]);
@@ -51,4 +51,4 @@ function collectStrings2(obj) {
   helper(obj);
   return result;
 }
-console.log(collectStrings2(obj)); //['foo','bar','baz']
+console.log(collectStrings1(obj)); //['foo','bar','baz']
