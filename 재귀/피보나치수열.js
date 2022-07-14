3dd65aae6fd5ea91630f1294ac6ee7cb8bdb222e
tpgus <tpgus758@naver.com>
2022-07-15 02:45:45 +0900

    피보나치 수열 : 메모이제이션

---
+++ 재귀/피보나치수열.js
@@ -0,0 +1,32 @@
+/*
+피보나치 수열은 첫 항이 어디부터 시작하는지 따져봐야 한다.
+0항부터 시작이라면 0 1 1 2 3 5 8 ...이고
+1항부터 시작이라면 1 1 2 3 5 8 ... 이고
+*/
+
+function fib(n) {
+  //1항부터 시작이라면,
+  if (n <= 2) {
+    return 1;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+//메모이제이션 : 1항부터 시작일 때,
+function fib(n) {
+  let memo = { 1: 1, 2: 1 };
+
+  function helper(n) {
+    if (memo[n]) {
+      return memo[n];
+    } else {
+      memo[n] = n;
+    }
+    return helper(n - 1) + helper(n - 2);
+  }
+
+  return helper(n);
+}
+
+console.log(fib(3));
