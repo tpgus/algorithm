e22c4564b8d82122cbc30a642db39cbf62852b8c
tpgus <tpgus758@naver.com>
2022-07-16 21:59:40 +0900

    설정 파일

---
+++ 정렬/test.js
@@ -0,0 +1,38 @@
+function merge(arr1, arr2) {
+  let result = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      result.push(arr1[i]);
+      i++;
+    } else {
+      result.push(arr2[j]);
+      j++;
+    }
+  }
+
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+
+  return result;
+}
+
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  let half = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, half));
+  let right = mergeSort(arr.slice(halr));
+  return merge(left, right);
+}
